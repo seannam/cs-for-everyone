07b6e98657202c02b31ab3cc8ca2219b8eaa8897
Sean Nam <namsangwoo1@gmail.com>
2013-12-21 00:41:34 +0900

    add lesson 7.2

---
+++ Lesson 5-1.c
@@ -0,0 +1,7 @@
+// Lesson 5.1 Writing Your First Program
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World");
+	return 0;
+}
---
+++ Lesson 7-2.c
@@ -0,0 +1,9 @@
+//7.2 The Connection Between Function Return Values and Variables
+
+int main(void) {
+	int total = 5;
+
+	printf("The total is %d", total);
+
+	return 0;
+}
