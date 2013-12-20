1001c12811e274e495f24a4fd471eca75c971f04
Sean Nam <namsangwoo1@gmail.com>
2013-12-21 00:46:10 +0900

    removed some files

--- 7.2.c
+++
@@ -1,9 +0,0 @@
-//7.2 The Connection Between Function Return Values and Variables
-
-int main(void) {
-	int total = 5;
-
-	printf("The total is %d %d %d %d", total, 8, total+3, printf("123"));
-
-	return 0;
-}
